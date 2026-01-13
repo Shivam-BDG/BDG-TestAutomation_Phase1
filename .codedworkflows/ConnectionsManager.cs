f70b3613c7c775485bd8eef72b8c6660f45d0557
BDG_TestAutomation <ssingh14@bdgrowers.com>
2026-01-13 03:18:38 -0800

    First Commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace BDG_TestAutomation
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
